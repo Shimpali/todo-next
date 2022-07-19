c6b9fe3e97569767409df154e4895d0cb4ea198f
Shimpali Deshpande <shimpali.deshpande@yahoo.com>
2022-07-19 17:05:09 +0530

    Added Home page

---
+++ components/TopBar.tsx
@@ -0,0 +1,30 @@
+import { Flex, Text } from '@chakra-ui/layout';
+
+const TopBar = () => (
+  <Flex
+    w="100%"
+    flexDirection="row"
+    alignContent="center"
+    p="8px 16px"
+    justifyContent="center"
+    bg="#091932"
+  >
+    <Flex
+      w={['100%', '100%', '100%', '90ch']}
+      flexDirection="row"
+      alignContent="center"
+    >
+      <Text
+        fontSize="36px"
+        fontWeight="bold"
+        lineHeight="42px"
+        color="#eef2f7"
+        flexGrow={1}
+      >
+        ToDo App
+      </Text>
+    </Flex>
+  </Flex>
+);
+
+export default TopBar;
